736ae223f72aae8b7895df07932539607ecd0cb1
raymond1688 <raymond@gfactor.com.tw>
2023-02-13 13:51:22 +0800

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+# Read the inputs from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", sum)
